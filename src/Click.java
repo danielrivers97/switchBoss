041e8e719dec2fcff1f87d459ef9ef50429f05f9
Matt <45921054+mbstalli@users.noreply.github.com>
2020-01-28 17:22:58 -0800

    1.0

---
+++ src/Click.java
@@ -0,0 +1,16 @@
+public class Click {
+    float x;
+    float y;
+
+    public Click() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+    public float getY() {
+        return this.y;
+    }
+}
