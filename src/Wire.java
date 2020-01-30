ad99ff3cebcd5f77b21496069f9999eb5cc5dc4e
Andrew Bartfeld <bartfeld.andrew@gmail.com>
2020-01-30 12:19:51 -0800

    updated some variables

---
+++ src/Wire.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class Wire {
+
+    protected PApplet sketch;
+
+    // only attributes needed are the two components the wire goes between!
+    // because of how it is rendered
+//    private Component to;
+//    private Component from;
+//
+    public void draw_wire(float scale, Component to, Component from) {
+
+    }
+
+}
