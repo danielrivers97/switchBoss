7202d453de7b890e1f1e7874b36afcf7b6d4c133
Andrew Bartfeld <bartfeld.andrew@gmail.com>
2020-02-04 13:44:05 -0800

    semi working?

--- src/Switch.java
+++ src/Switch.java
@@ -13,36 +13,36 @@ public class Switch extends Component {
 
         switch (getOrientation()) {
             case 0: // N
-                setHeight(3 * UNIT);
-                setWidth(2 * UNIT);
-                setInX(x + UNIT);
-                setOutX(x + UNIT);
-                setInY(y + (3 * UNIT));
+                setHeight(3);
+                setWidth(2);
+                setInX(x + 1);
+                setOutX(x + 1);
+                setInY(y + 3);
                 setOutY(y);
                 break;
             case 1: // E
-                setHeight(2 * UNIT);
-                setWidth(3 * UNIT);
+                setHeight(2);
+                setWidth(3);
                 setInX(x);
-                setOutX(x + (3 * UNIT));
-                setInY(y + UNIT);
-                setOutY(y + UNIT);
+                setOutX(x + 3);
+                setInY(y + 1);
+                setOutY(y + 1);
                 break;
             case 2: // S
-                setHeight(3 * UNIT);
-                setWidth(2 * UNIT);
-                setInX(x + UNIT);
-                setOutX(x + UNIT);
+                setHeight(3);
+                setWidth(2);
+                setInX(x + 1);
+                setOutX(x + 1);
                 setInY(y);
-                setOutY(y + (3 * UNIT));
+                setOutY(y + 3);
                 break;
             case 3: // W
-                setHeight(2 * UNIT);
-                setWidth(3 * UNIT);
-                setInX(x + (3 * UNIT));
+                setHeight(2);
+                setWidth(3);
+                setInX(x + 3);
                 setOutX(x);
-                setInY(y + UNIT);
-                setOutY(y + UNIT);
+                setInY(y + 1);
+                setOutY(y + 1);
                 break;
             default:
                 System.exit(-1); // error checks later (SHOULD NEVER HAPPEN)
@@ -50,36 +50,25 @@ public class Switch extends Component {
     }
 
     public void render(float scale, int panX, int panY) {
-        int unit = (int) (UNIT * scale);
-        int x = (int) (scale * (getX() + panX));
-        int y = (int) (scale * (getY() + panY));
-        int outX = (int) (scale * (getOutX() + panX));
-        int outY = (int) (scale * (getOutY() + panY));
+        int unit = (int) (UNIT * scale); // grid unit with scale taken into account
+        int x = calcPos(getX(), scale, panX);
+        int y = calcPos(getY(), scale, panY);
 
         super.render(scale, panX, panY);
 
+        sketch.stroke(0, 0, 0);
         sketch.strokeWeight(3 * scale);
         if (getOrientation() == 0 || getOrientation() == 2) { // N/S facing
             sketch.line(x + unit, y, x + unit, y + unit);
             sketch.line(x + unit, y + 2 * unit, x + unit, y + 3 * unit);
             sketch.line(x, y + unit, x + 2 * unit, y + unit);
             sketch.line(x, y + 2 * unit, x + 2 * unit, y + 2 * unit);
-            if (getOrientation() == 0) {
-                sketch.triangle(outX, outY, (int) (outX + 2.5 * scale), outY + 5 * scale, (int) (outX - 2.5 * scale), outY + 5 * scale);
-            } else {
-                sketch.triangle(outX, outY, (int) (outX + 2.5 * scale), outY - 5 * scale, (int) (outX - 2.5 * scale), outY - 5 * scale);
 
-            }
         } else { // E / W facing
             sketch.line(x, y + unit, x + unit, y + unit);
             sketch.line(x + 2 * unit, y + unit, x + 3 * unit, y + unit);
             sketch.line(x + unit, y, x + unit, y + 2 * unit);
             sketch.line(x + 2 * unit, y, x + 2 * unit, y + 2 * unit);
-            if (getOrientation() == 1) {
-                sketch.triangle(outX, outY,outX - 5 * scale, (int) (outY + 2.5 * scale), outX - 5 * scale, (int) (outY - 2.5 * scale));
-            } else {
-                sketch.triangle(outX, outY,outX + 5 * scale, (int) (outY + 2.5 * scale), outX + 5 * scale, (int) (outY - 2.5 * scale));
-            }
         }
     }
 
