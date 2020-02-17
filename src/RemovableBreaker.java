9fcd472e24585c4650c18d60486432da1a9d71c9
Matt <45921054+mbstalli@users.noreply.github.com>
2020-02-17 14:44:16 -0800

    removed disappearing piece when clicked

--- src/RemovableBreaker.java
+++ src/RemovableBreaker.java
@@ -20,20 +20,18 @@ public class RemovableBreaker extends Component{
             sketch.line(x + unit, y + 4 * unit, x + 2 * unit, y + 7 * unit/2); //bottom outside arrow
             sketch.line(x + unit, y + 4 * unit, x, y + 7 * unit/2);
 
-            if(this.getCurrentstate() == this.getNormalstate()) {
-                sketch.line(x + unit, y - unit/2, x + 2 * unit, y); //top inside arrow
-                sketch.line(x + unit, y - unit/2, x, y);
+            sketch.line(x + unit, y - unit/2, x + 2 * unit, y); //top inside arrow
+            sketch.line(x + unit, y - unit/2, x, y);
 
-                sketch.line(x + unit, y + 7 * unit/2, x + 2 * unit, y + 3 * unit); //bottom inside arrow
-                sketch.line(x + unit, y + 7 * unit/2, x, y + 3 * unit);
+            sketch.line(x + unit, y + 7 * unit/2, x + 2 * unit, y + 3 * unit); //bottom inside arrow
+            sketch.line(x + unit, y + 7 * unit/2, x, y + 3 * unit);
 
-                sketch.line(x + unit, y - unit/2, x + unit, y + unit/2); //top vertical line
-                sketch.line(x + unit, y + 5* unit/2, x + unit, y + 7 * unit/2); //bottom vertical line
-                sketch.line(x, y + unit/2, x + 2 * unit, y + unit/2); //top horizontal line
-                sketch.line(x, y + 5 * unit/2, x + 2 * unit, y + 5 * unit/2); //bottom horizontal line
-                sketch.line(x, y + unit/2, x, y + 5*unit/2); //left vertical line
-                sketch.line(x + 2 * unit, y + unit/2, x + 2 * unit, y + 5 * unit/2); //right vertical line
-            }
+            sketch.line(x + unit, y - unit/2, x + unit, y + unit/2); //top vertical line
+            sketch.line(x + unit, y + 5* unit/2, x + unit, y + 7 * unit/2); //bottom vertical line
+            sketch.line(x, y + unit/2, x + 2 * unit, y + unit/2); //top horizontal line
+            sketch.line(x, y + 5 * unit/2, x + 2 * unit, y + 5 * unit/2); //bottom horizontal line
+            sketch.line(x, y + unit/2, x, y + 5*unit/2); //left vertical line
+            sketch.line(x + 2 * unit, y + unit/2, x + 2 * unit, y + 5 * unit/2); //right vertical line
         }
         else { // E / W facing
             sketch.line(x - unit, y + unit, x - unit/2, y); //left outside arrow
@@ -42,20 +40,18 @@ public class RemovableBreaker extends Component{
             sketch.line(x + 4 * unit, y + unit, x + 7 * unit/2, y + 2 * unit); //right outside arrow
             sketch.line(x + 4 * unit, y + unit, x + 7 * unit/2, y);
 
-            if(this.getCurrentstate() == this.getNormalstate()) {
-                sketch.line(x - unit/2, y + unit, x, y); //left inside arrow
-                sketch.line(x - unit/2, y + unit, x, y + 2 * unit);
+            sketch.line(x - unit/2, y + unit, x, y); //left inside arrow
+            sketch.line(x - unit/2, y + unit, x, y + 2 * unit);
 
-                sketch.line(x + 7 * unit/2, y + unit, x + 3 * unit, y); //right inside arrow
-                sketch.line(x + 7 * unit/2, y + unit, x + 3 * unit, y + 2 * unit);
+            sketch.line(x + 7 * unit/2, y + unit, x + 3 * unit, y); //right inside arrow
+            sketch.line(x + 7 * unit/2, y + unit, x + 3 * unit, y + 2 * unit);
 
-                sketch.line(x - unit/2, y + unit, x + unit/2, y + unit); //left horizontal line
-                sketch.line(x + 5 * unit/2, y + unit, x + 7 * unit/2, y + unit); //right horizontal line
-                sketch.line(x + unit/2, y, x + unit/2, y + 2 * unit); //left vertical line
-                sketch.line(x + 5 * unit/2, y, x + 5 * unit/2, y + 2 * unit); //right vertical line
-                sketch.line(x + unit/2, y, x + 5 * unit/2, y); //top horizontal line
-                sketch.line(x + unit/2, y + 2 * unit, x + 5 * unit/2, y + 2 * unit); //bottom horizontal line
-            }
+            sketch.line(x - unit/2, y + unit, x + unit/2, y + unit); //left horizontal line
+            sketch.line(x + 5 * unit/2, y + unit, x + 7 * unit/2, y + unit); //right horizontal line
+            sketch.line(x + unit/2, y, x + unit/2, y + 2 * unit); //left vertical line
+            sketch.line(x + 5 * unit/2, y, x + 5 * unit/2, y + 2 * unit); //right vertical line
+            sketch.line(x + unit/2, y, x + 5 * unit/2, y); //top horizontal line
+            sketch.line(x + unit/2, y + 2 * unit, x + 5 * unit/2, y + 2 * unit); //bottom horizontal line
         }
     }
 }
