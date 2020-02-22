c3aaafac1ae44271cfc53da757dcdee36afdb100
Kuserab <rmcook@calpoly.edu>
2020-02-22 11:47:49 -0800

    started tools ui

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -10,8 +10,14 @@ public class UserInterface {
     private float titleX = 15;
     private float titleY = 30;
 
+    // pixel location of lower right corner
+    private float toolsX = 1450;
+    private float toolsY = 600;
+
     private float titleWidth;
     private float titleHeight;
+    private float toolsWidth;
+    private float toolsHeight;
 
     protected SwitchBoss sketch;
 
@@ -25,6 +31,8 @@ public class UserInterface {
         this.screenHeight = sketch.HEIGHT * sketch.UNIT;
         this.titleWidth = screenWidth / 5;
         this.titleHeight = screenHeight / 20;
+        this.toolsWidth = screenWidth / 30;
+        this.toolsHeight = screenHeight / 5;
     }
 
     public void draw() {
@@ -35,6 +43,12 @@ public class UserInterface {
         sketch.fill(0);
         sketch.textSize(titleSize);
         sketch.text(title, titleX + buffer, titleY + buffer, titleX + titleWidth, titleY + titleHeight);
+
+        sketch.stroke(0);
+        sketch.fill(255);
+        sketch.strokeWeight(1);
+        sketch.rect(toolsX, toolsY, toolsWidth, toolsHeight);
+        sketch.fill(0);
 
     }
 
